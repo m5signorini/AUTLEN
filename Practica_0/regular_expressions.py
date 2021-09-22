eaa0faa84eb315515513859abed156ba7005831d
Martín <mssignorini@gmail.com>
2021-09-22 16:58:24 +0200

    P0 - Ej:1,2,3

---
+++ Practica_0/regular_expressions.py
@@ -0,0 +1,29 @@
+"""
+Esta es la expresion regular para el ejercicio 0, que se facilita
+a modo de ejemplo:
+"""
+RE0 = "[a-zA-Z]+"
+
+"""
+Completa a continuacion las expresiones regulares para los
+ejercicios 1-5:
+"""
+RE1 = "([a-zA-Z]|_|[0-9])+\.py"
+RE2 = "-?(((([1-9][0-9]*)|0)(\.[0-9]*)?)|(\.[0-9]+))"
+RE3 = "[a-z]+\.[a-z]+@(estudiante\.)?uam\.es"
+RE4 = ""
+RE5 = ""
+
+"""
+Recuerda que puedes usar el fichero prueba.py para probar tus
+expresiones regulares.
+"""
+
+"""
+EJERCICIO 6:
+Incluye a continuacion, dentro de esta cadena, tu respuesta
+al ejercicio 6.
+
+... tu respuesta aqui...
+
+"""
