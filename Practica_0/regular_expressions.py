de9c0d495dbf69564c93e73c9fb746b492559894
Martín <mssignorini@gmail.com>
2021-09-22 17:16:14 +0200

    Ej: 1,2,3,4,5

--- Practica_0/regular_expressions.py
+++ Practica_0/regular_expressions.py
@@ -8,11 +8,16 @@ RE0 = "[a-zA-Z]+"
 Completa a continuacion las expresiones regulares para los
 ejercicios 1-5:
 """
+_except_parenthesis = "[^\(\)]*"
 RE1 = "([a-zA-Z]|_|[0-9])+\.py"
 RE2 = "-?(((([1-9][0-9]*)|0)(\.[0-9]*)?)|(\.[0-9]+))"
 RE3 = "[a-z]+\.[a-z]+@(estudiante\.)?uam\.es"
-RE4 = ""
-RE5 = ""
+# Alternativa: ([^\(\)]*\([^\(\)]*\)[^\(\)]*)+
+RE4 = "(" + _except_parenthesis + "\(" + _except_parenthesis + "\)" + _except_parenthesis + ")+"
+# Alternativa: (([^\(\)]*\([^\(\)]*\)[^\(\)]*)+|[^\(\)]*)
+_max_one_parenthesis = "(" + RE4 + "|" + _except_parenthesis + ")"
+# Alternativa: ([^\(\)]*\((([^\(\)]*\([^\(\)]*\)[^\(\)]*)+|[^\(\)]*)\)[^\(\)]*)+
+RE5 = "(" + _except_parenthesis + "\(" + _max_one_parenthesis + "\)" + _except_parenthesis + ")+"
 
 """
 Recuerda que puedes usar el fichero prueba.py para probar tus
