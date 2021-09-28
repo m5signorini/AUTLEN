5b941684fb38e5c725a8d33b61bf5ab6c6857a4b
juancavr <40234290+juancavr@users.noreply.github.com>
2021-09-28 12:32:13 +0200

    Add files via upload

---
+++ Practica_0/regular_expressions_jc.py
@@ -0,0 +1,29 @@
+"""
+Esta es la expresion regular para el ejercicio 0, que se facilita
+a modo de ejemplo:
+"""
+RE0 = "[a-zA-Z]+"
+
+"""
+Completa a continuacion las expresiones regulares para los
+ejercicios 1-5:
+"""
+RE1 = "[a-zA-Z1-9_]+(.py$)"
+RE2 = "(-?[1-9]+(\.)?[0-9]*)|(-?0\.[0-9]*)|(-?0)|(-?\.[0-9]*)"
+RE3 = "[a-z]+\.[a-z]+\@((uam\.es)|(estudiante\.uam\.es))"
+RE4 = "([\w ]*(\([a-z ]*\)[ \w]*))+"
+RE5 = "(([\w ]*\(([\w ]*(\([a-z ]*\)[ \w]*))*\)[\w ]*)|([\w ]*(\([a-z ]*\)[ \w]*)))+"
+
+"""
+Recuerda que puedes usar el fichero prueba.py para probar tus
+expresiones regulares.
+"""
+
+""" 
+EJERCICIO 6:
+Incluye a continuacion, dentro de esta cadena, tu respuesta 
+al ejercicio 6.
+
+... tu respuesta aqui...
+
+"""
